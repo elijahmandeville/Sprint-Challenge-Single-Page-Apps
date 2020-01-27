18fe1fce2a6f9ef80218557034fea0197905e7b2
elijahmandeville <elijahmandeville@gmail.com>
2020-01-26 18:23:55 -0700

    finished welcome and main characters page, stuck on search

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [ chars, setChars ] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then(res => {
+      setChars(res.data.results);
+      // console.log(res.data.results);
+    })
+    .catch(err => console.log('error: ', err));
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h1>Character List</h1>
+      <SearchForm />
+      <br />
+      {chars.map((e, i) => {
+        return <CharacterCard key={i} character={e} />
+      })}
     </section>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import { useHistory } from "react-router-dom";
+
 
 export default function SearchForm() {
- 
+  const [ name, setName ] = useState("");
+  const [ searchedName, setSearchedName ] = useState("");
+  let history = useHistory();
+
+  useEffect(() => {
+    axios
+    .get(`https://rickandmortyapi.com/api/character/?${name}=rick&status=alive`)
+    .then()
+    .catch(err => console.log('error: ', err))
+  }, [name])
+
+  // const onSubmitHandler = (e) => {
+  //   e.preventDefault();
+    
+  // }
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        setName(e.target.value);
+        // console.log(name)
+      }}>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <button type="submit" disabled={!name}>
+          Search
+        </button>
+      </form>
     </section>
   );
-}
+  }
